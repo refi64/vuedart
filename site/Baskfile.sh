0173490a0f3883ea8d25d55f74565249c828837d
Ryan Gonzalez <rymg19@gmail.com>
2017-11-19 20:04:45 -0600

    Site fixes + aspen migration

--- site/Baskfile.sh
+++ site/Baskfile.sh
@@ -7,25 +7,6 @@ task_default() {
 }
 
 
-pack() {
-  mkdir -p web/dist
-  local mode="$1"
-
-  if [ "$mode" == "debug" ]; then
-    local vue=vue.js
-    local vuematerial=vue-material.debug.js
-  else
-    local vue=vue.min.js
-    local vuematerial=vue-material.js
-  fi
-
-  cat node_modules/vue/dist/$vue node_modules/vue-material/dist/$vuematerial > \
-    web/dist/pack.js
-  cat node_modules/vue-material/dist/vue-material.css web/dist/pygments.css > \
-    web/dist/pack.css
-}
-
-
 task_pygments() {
   pygmentize -S friendly -f html -a .highlight > web/dist/pygments.css
 }
@@ -33,13 +14,13 @@ task_pygments() {
 
 task_pack_debug() {
   bask_depends pygments
-  pack debug
+  aspen
 }
 
 
 task_pack_release() {
   bask_depends pygments
-  pack release
+  aspen -m prod
 }
 
 task_build_debug() {
--- site/lib/common.dart
+++ site/lib/common.dart
@@ -1,7 +1,11 @@
+import 'package:aspen_assets/aspen_assets.dart' as aspen;
 import 'package:vue2/plugins/vuematerial.dart';
 
 
 void setup() {
+  aspen.loadGlobal('pygments-css');
+  aspen.loadGlobal('vue-material-css');
+
   VueMaterial.use();
   VueMaterial.registerTheme('main', new MdTheme(
     primary: new MdColor(color: 'blue-grey', hue: 900),
